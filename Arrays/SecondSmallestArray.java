864f6e80654240b10a47df608d8bc71ef5570ef3
Aniket-More-19 <aniket19m@gmail.com>
2024-04-06 12:41:00 +0530

    trial commit

--- Arrays/SecondSmallestArray.java
+++ Arrays/SecondSmallestArray.java
@@ -6,5 +6,6 @@ public class SecondSmallestArray {
     public static void main(String[] args) {
         int arr[] = {11, 2, 45, 15, 5, 10};
 
+        // sort array and get the 2nd element
     }
 }
